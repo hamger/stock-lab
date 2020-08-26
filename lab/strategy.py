7523d8165b69d79e23f6ae3862627cfe3f53c2c6
hamger <2313303800@qq.com>
2020-08-26 09:06:04 +0800

    update

--- lab/strategy.py
+++ lab/strategy.py
@@ -18,12 +18,12 @@ class Strategy(object):
     def strategy_1(self):
         res = []
         for index, row in enumerate(self.data):
-            # if index > 100:
-            #     break
+            if index > 100000:
+                break
 
             rs = bs.query_history_k_data_plus(row,
                                               "date,code,open,high,low,close,preclose,pctChg,isST",
-                                              start_date='2020-08-20', end_date='2020-08-24',
+                                              start_date='2020-08-21', end_date='2020-08-25',
                                               frequency="d", adjustflag="2")
 
             tmp = []
@@ -32,14 +32,14 @@ class Strategy(object):
                 df = rs.get_row_data()
                 if df[8] == 1:
                     continue
-                # 条件1：当天为涨幅大于2%
+                # 条件1：当天为涨幅大于1%
                 condition_1 = float(df[7]) >= 1
                 # # 条件2：最大跌幅不超过1%
                 # condition_2 = (float(df[2]) - float(df[4])) / float(df[2]) < 1 * 0.01
-                # # 条件3：收盘价为最高价
-                # condition_3 = float(df[5]) == float(df[3])
-                if condition_1:
-                    # if condition_1 and condition_2 and condition_3:
+                # 条件3：收盘价为最高价
+                condition_3 = float(df[5]) == float(df[3])
+                if condition_1 and condition_3:
+                # if condition_1 and condition_2 and condition_3:
                     tmp.append(df[7])
             # 连续3天满足要求
             if len(tmp) >= 3:
